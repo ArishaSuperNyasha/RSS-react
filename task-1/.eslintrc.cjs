54a8d77cd8fb58998c1e411ed1184d4fd566ac7d
ArishaSuperNyasha <evangelionyouthresort@gmail.com>
2024-07-02 17:17:09 +0300

    feat: set up react, ts, eslint and add eslint rules

--- task-1/.eslintrc.cjs
+++ task-1/.eslintrc.cjs
@@ -5,14 +5,22 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'eslint-config-prettier'
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: [
+    'react-refresh',
+    'react-compiler',
+    'eslint-plugin-prettier'
+  ],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    '@typescript-eslint/no-explicit-any': 'error',
+    'react-compiler/react-compiler': 'error',
+    'prettier/prettier': 'error',
   },
 }
