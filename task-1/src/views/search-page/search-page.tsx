2f2bcfecc772eea05d3704d26cc3e4c3265c6902
ArishaSuperNyasha <evangelionyouthresort@gmail.com>
2024-07-04 20:18:58 +0300

    feat: add Search Page to App

---
+++ task-1/src/views/search-page/search-page.tsx
@@ -0,0 +1,11 @@
+import { Component, ReactNode } from 'react';
+
+export class SearchPage extends Component {
+  render(): ReactNode {
+    return (
+      <>
+        <h1>Страница поиска</h1>
+      </>
+    );
+  }
+}
