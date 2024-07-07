0dcee98e50c59fccb756afb5107832ea7a83dce7
ArishaSuperNyasha <evangelionyouthresort@gmail.com>
2024-07-07 21:52:08 +0300

    fix: fix bug when there is only one charData in response

--- task-1/src/services/interfaces.ts
+++ task-1/src/services/interfaces.ts
@@ -1,20 +1,22 @@
+export type CharData = {
+  allies: string[];
+  createdAt: string;
+  enemies: string[];
+  films: string[];
+  imageUrl: string;
+  name: string;
+  parkAttractions: string[];
+  shortFilms: string[];
+  sourceUrl: string;
+  tvShows: string[];
+  updatedAt: string;
+  url: string;
+  videoGames: string[];
+  _id: number;
+};
+
 export type AllCharsData = {
-  data: {
-    allies: string[];
-    createdAt: string;
-    enemies: string[];
-    films: string[];
-    imageUrl: string;
-    name: string;
-    parkAttractions: string[];
-    shortFilms: string[];
-    sourceUrl: string;
-    tvShows: string[];
-    updatedAt: string;
-    url: string;
-    videoGames: string[];
-    _id: number;
-  }[];
+  data: CharData[] | CharData;
   info: {
     count: number;
     nextPage: string | null;
