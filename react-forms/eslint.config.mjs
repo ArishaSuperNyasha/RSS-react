fc182818ca7dc7d15c84e566b7d21f26559f2cbe
ArishaSuperNyasha <evangelionyouthresort@gmail.com>
2024-08-15 16:03:02 +0300

    feat: add prettier and eslint

---
+++ react-forms/eslint.config.mjs
@@ -0,0 +1,51 @@
+import { fixupConfigRules } from "@eslint/compat";
+import reactRefresh from "eslint-plugin-react-refresh";
+import reactCompiler from "eslint-plugin-react-compiler";
+import prettier from "eslint-plugin-prettier";
+import globals from "globals";
+import tsParser from "@typescript-eslint/parser";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default [{
+    ignores: ["**/dist", "**/.eslintrc.cjs"],
+}, ...fixupConfigRules(compat.extends(
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+    "eslint-config-prettier",
+)), {
+    plugins: {
+        "react-refresh": reactRefresh,
+        "react-compiler": reactCompiler,
+        prettier,
+    },
+
+    languageOptions: {
+        globals: {
+            ...globals.browser,
+        },
+
+        parser: tsParser,
+    },
+
+    rules: {
+        "react-refresh/only-export-components": ["warn", {
+            allowConstantExport: true,
+        }],
+
+        "@typescript-eslint/no-explicit-any": "error",
+        "react-compiler/react-compiler": "error",
+        "prettier/prettier": "error",
+    },
+}];
